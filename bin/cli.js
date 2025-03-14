faa7b6820f7a635168bdd78a1ee8c577ff7b1e8f
Matt <mfrey@stokespace.com>
2025-03-14 15:27:03 -0600

    Fix bin script

---
+++ bin/cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+// Get the directory of the current script
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Path to the built index.js file
+const serverPath = resolve(__dirname, '../build/index.js');
+
+// Spawn the supergateway process
+const gateway = spawn('npx', [
+  '-y',
+  'supergateway',
+  '--port',
+  '6060',
+  '--stdio',
+  `node ${serverPath}`
+], {
+  stdio: 'inherit',
+  shell: true
+});
+
+// Handle process exit
+process.on('SIGINT', () => {
+  gateway.kill('SIGINT');
+});
+
+gateway.on('close', (code) => {
+  process.exit(code);
+}); 
