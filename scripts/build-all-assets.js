5d63ec6a5ea791b949e2d7a8492e4dd57e78ebad
Matt <matthewfrey118@gmail.com>
2025-09-01 19:10:05 +0000

    fix(cd): michael jackson script

--- scripts/build-all-assets.js
+++ scripts/build-all-assets.js
@@ -1,4 +1,4 @@
-const { execSync } = require('child_process');
+import { execSync } from 'node:child_process'
 
 console.log('🔨 Running DXT build as part of semantic-release prepare step...');
 execSync('npm run build:dxt', { stdio: 'inherit' });
